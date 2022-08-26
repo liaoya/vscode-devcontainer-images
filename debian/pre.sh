6d90227c237fcd35c342e3a21750d020338c9d14
Huifeng Shen <liaoya@gmail.com>
2022-08-26 11:23:29 +0800

    Use debian slim image

--- debian/pre.sh
+++ debian/pre.sh
@@ -3,6 +3,6 @@
 if [[ -z ${DEBIAN_MIRROR_PATH+x} ]]; then
     DEBIAN_MIRROR_PATH=http://mirrors.ustc.edu.cn
 fi
-export BASE_IMAGE=${BASE_IMAGE:-docker.io/library/debian:bullseye}
+export BASE_IMAGE=${BASE_IMAGE:-docker.io/library/debian:bullseye-slim}
 DEBIAN_VERSION=$(echo "${BASE_IMAGE}" | cut -d":" -f2)
 add_image "docker.io/yaekee/devcontainer-base:debian-${DEBIAN_VERSION}"
