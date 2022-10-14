2bfe844257035cc18c910bad89a87363f7ee3dd7
Huifeng Shen <liaoya@gmail.com>
2022-10-14 17:46:21 +0800

    Fix Dockerfile

--- meson/pre.sh
+++ meson/pre.sh
@@ -17,7 +17,7 @@ NINJA_VERSION=${NINJA_VERSION:-$(curl -sL https://api.github.com/repos/ninja-bui
 NINJA_VERSION=${NINJA_VERSION:-v1.11.1}
 export NINJA_VERSION
 
-MESON_VERSION=${MESON_VERSION:-$(curl -sL https://api.github.com/repos/mesonbuild/meson/releases | jq -r -S ".[].tag_name" | grep -v '\-rc' | sort -r | head -n 1)}
+MESON_VERSION=${MESON_VERSION:-$(curl -sL https://api.github.com/repos/mesonbuild/meson/releases | jq -r -S ".[].tag_name" | grep -v 'rc' | sort -r | head -n 1)}
 MESON_VERSION=${MESON_VERSION:-0.63.3}
 export MESON_VERSION
 
